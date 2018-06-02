0f24ff0e3b6c6d8252239334368521d503d44911
KJlmfe <kjlmfe@gmail.com>
2018-06-02 21:45:13 +0800

    refactor: use k8s secret to set env instead of mounting config/
    __dirname is not correct in files of k8s volumeMounts

--- config/config.default.js
+++ config/config.default.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const env = process.env;
+
 module.exports = appInfo => {
   const config = exports = {};
 
@@ -9,14 +11,14 @@ module.exports = appInfo => {
   // add your config here
   config.middleware = [];
 
-  const POSTGRES_DB_HOST = process.env.POSTGRES_DB_HOST || '';
+  const POSTGRES_DB_HOST = env.POSTGRES_DB_HOST || '';
   config.sequelize = {
     dialect: 'postgres',
     database: 'smart-signature-pro',
     host: POSTGRES_DB_HOST.split(':')[0] || 'localhost',
     port: POSTGRES_DB_HOST.split(':')[1] || '5432',
-    username: process.env.POSTGRES_DB_USER || 'postgres',
-    password: process.env.POSTGRES_DB_PASSWORD || 'postgres',
+    username: env.POSTGRES_DB_USER || 'postgres',
+    password: env.POSTGRES_DB_PASSWORD || 'postgres',
   };
 
   config.security = {
@@ -27,11 +29,11 @@ module.exports = appInfo => {
 
   config.smsVerify = {
     ttl: 10, // 验证码有效期（分钟）
-    mock: '1234', // 如果设置了mock，就不会真正去发短信
+    mock: env.SMS_MOCK, // 如果设置了mock，就不会真正去发短信
     provider: {
       type: 'yunpian',
       api: 'https://sms.yunpian.com/v2/sms/single_send.json',
-      key: '<REPLACE_WITH_YOUR_KEY>',
+      key: env.YUNPIAN_KEY || '<REPLACE_WITH_YOUR_KEY>',
       template: '【云片网】您的验证码是',
     },
   };
